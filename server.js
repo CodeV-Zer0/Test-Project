2777e688bf08d43df12f3275d1ff294eab827f1f
Vignesh <samathamvignesh73@gmail.com>
2026-06-11 08:09:00 +0530

    Adding Sitemap

--- server.js
+++ server.js
@@ -297,6 +297,21 @@ app.delete("/api/reviews/:id", async (req, res) => {
     res.json({ success: true });
 });
  
+//===SITEMAP===
+app.get("/sitemap.xml", (req, res) => {
+    res.header("Content-Type", "application/xml");
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.walktheprimrosepath.com/</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.walktheprimrosepath.com/plant/</loc>
+    <priority>0.8</priority>
+  </url>
+</urlset>`);
+});
 // ===== PAGE ROUTES =====
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "admin.html")));
